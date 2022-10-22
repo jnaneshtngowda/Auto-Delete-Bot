4f5437fc8ca2f62dc5b7528bae3c6a7324329567
jnaneshtngowda <114807226+jnaneshtngowda@users.noreply.github.com>
2022-10-22 23:19:35 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ User = Client(name="user-account",
               session_string=SESSION,
               api_id=API_ID,
               api_hash=API_HASH,
-              workers=300
+              workers=3600
               )
 
 
@@ -29,7 +29,7 @@ Bot = Client(name="auto-delete",
              api_id=API_ID,
              api_hash=API_HASH,
              bot_token=BOT_TOKEN,
-             workers=300
+             workers=3600
              )
 
 
